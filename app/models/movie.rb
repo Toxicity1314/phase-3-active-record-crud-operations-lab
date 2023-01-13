a4f37bfbc33325d661f3866a6104cf083c146eee
Sean Welsh <s.welsh1314@gmail.com>
2023-01-13 11:52:38 -0500

    getting closer

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,30 @@
 class Movie < ActiveRecord::Base
 
-    self.create_with_title do
-        self.create(:title)
+    def self.create_with_title title
+        create(title: title)
     end
+
+   def self.first_movie
+    first
+   end
+
+   def self.last_movie
+    last
+   end
+
+   def self.movie_count
+    count
+   end
+
+   def self.find_movie_with_id id
+    find(id)
+   end
+
+   def self.find_movie_with_attributes attribute
+    find_by()
+   end
+
+
 
 
 end
